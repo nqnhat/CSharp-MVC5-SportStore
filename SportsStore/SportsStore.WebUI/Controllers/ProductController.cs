e82c359fbbe40a38315be94c0caf9ef3da302748
GuangRi <GuangRi@DESKTOP-5H74TOH>
2018-10-18 11:39:35 +0200

    Page 168

---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository _productInjection)
+        {
+            this.repository = _productInjection;
+        }
+
+        // GET: Product
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Ninject;
+using System.Web.Mvc;
+
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using Moq;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            this.kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            List<Product> mockProducts = new List<Product>();
+
+            mockProducts.Add(new Product { Name = "Football", Price = 25 });
+            mockProducts.Add(new Product { Name = "Surf board", Price = 179});
+            mockProducts.Add(new Product { Name = "Running shoes", Price = 95});
+
+            mock.Setup(i => i.Products).Returns(mockProducts);
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
